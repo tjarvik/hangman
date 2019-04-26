4c426d659920f415b6d19bab74ae066638d86a25
Tyler Jarvik <tjarvik@gmail.com>
2019-04-25 17:42:49 -0700

    refactor for game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,60 +1,16 @@
 
 class Game
-    @@MAX_GUESSES = 10
+    attr_accessor :letters_guessed
+    attr_accessor :wrong_letters
+    attr_reader :word
 
     def initialize(mode)
         if mode =~ /S/i
-            #load
+            load
         else
             @word = pick_word
             @letters_guessed = {}
             @wrong_letters = 0
-        end
-        display_word
-        until @wrong_letters == @@MAX_GUESSES
-            take_turn
-        end
-        puts "Hangman dies. The word was #{@word.join}"
-    end
-
-    def take_turn
-        input = ""
-        until input =~ /^[A-Z]$/i || input =~ /SAVE/i
-            puts "Guess a letter or type SAVE to save game."
-            input = gets.chomp
-        end
-        input = input.upcase
-        save_game if input =~ /SAVE/
-        if @letters_guessed.has_key?(input)
-            puts "You already guessed #{input}!"
-            return
-        end
-        @letters_guessed[input] = ""
-
-        display_word(input)
-    end
-
-    def display_word(guess = "")
-        correct = false
-        blanks = false
-        display_string = ""
-        @word.each do |letter|
-            if guess == letter
-                display_string += "#{letter} "
-                correct = true
-            elsif @letters_guessed.has_key?(letter)
-                display_string += "#{letter} "
-            else
-                display_string += "_ "
-                blanks = true
-            end
-        end
-        @wrong_letters += 1 unless correct && guess != ""
-        display_string += " (#{@@MAX_GUESSES - @wrong_letters} guesses remaining)"
-        puts display_string
-        unless blanks
-            puts "You win!"
-            exit
         end
     end
 
@@ -70,13 +26,72 @@ class Game
         choices[rand(0..(choices.length - 1))].upcase.split("")
     end
 
-    def load
+    def load###
+        @word = pick_word
+        @letters_guessed = {}
+        @wrong_letters = 0
+    end
+end
 
+class UI
+    @@MAX_GUESSES = 10
+
+    def initialize
+        mode = ""
+        until mode =~ /^[NS]$/i
+            puts "Please press N for new game or S to load saved game."
+            mode = gets.chomp
+        end
+
+        @game = Game.new(mode)
+        display_word
+        take_turn until @game.wrong_letters == @@MAX_GUESSES
+        puts "Hangman dies. The word was #{@game.word.join}"
+    end
+
+    def display_word(guess = "")
+        correct = false
+        blanks = false
+        display_string = ""
+        @game.word.each do |letter|
+            if guess == letter
+                display_string += "#{letter} "
+                correct = true
+            elsif @game.letters_guessed.has_key?(letter)
+                display_string += "#{letter} "
+            else
+                display_string += "_ "
+                blanks = true
+            end
+        end
+        @game.wrong_letters += 1 unless correct && guess != ""
+        display_string += " (#{@@MAX_GUESSES - @game.wrong_letters} guesses remaining)"
+        puts display_string
+        unless blanks
+            puts "You win!"
+            exit
+        end
+    end
+
+    def take_turn
+        input = ""
+        until input =~ /^[A-Z]$/i || input =~ /SAVE/i
+            puts "Guess a letter or type SAVE to save game."
+            input = gets.chomp
+        end
+        input = input.upcase
+        save_game if input =~ /SAVE/
+        if @game.letters_guessed.has_key?(input)
+            puts "You already guessed #{input}!"
+            return
+        end
+        @game.letters_guessed[input] = ""
+        display_word(input)
     end
 
     def save_game
         id = ""###
-        game_data = ""###
+        game_data = ""###word, letters guessed, wrong letters
         Dir.mkdir("saved_games") unless Dir.exists? "saved_games"
         filename = "saved_games/game_#{id}"
         File.open(filename,'w') do |file|
@@ -84,17 +99,6 @@ class Game
         end
         exit
     end
-end
-
-
-class UI
-    mode = ""
-    until mode =~ /^[NS]$/i
-        puts "Please press N for new game or S to load saved game."
-        mode = gets.chomp
-    end
-
-    Game.new(mode)
 end
 
 UI.new
